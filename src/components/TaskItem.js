b4590f700f40ca9d5c8c0c5fc5d7105db4d75c2a
Dirk Breeuwer <departdisclose@protonmail.com>
2023-01-21 08:51:10 +0000

    feat: Add double click WIP functionality to edit tasks

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,13 +1,24 @@
 import styles from "./TaskItem.module.css";
 
+import useDoubleClick from "../hooks/useDoubleClick";
+
 const TaskItem = (props) => {
+  const handleViewClick = useDoubleClick(
+    () => {
+      console.log("Single click");
+    },
+    () => {
+      console.log("Double click");
+    }
+  );
+
   return (
     <div className={styles.task}>
       <div><div
                 className={styles.checkbox}
                 onClick={props.onDeleteTask}
               /></div>
-      <span className={styles.description}>{props.description}</span>
+      <span className={styles.description} onClick={handleViewClick}>{props.description}</span>
     </div>
   );
 };
